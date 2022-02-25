7b6c1ce7a6ec89b4fa0209706e5052fff28296d3
Carlaav <carla2002.av@gmail.com>
2022-02-25 18:18:02 +0100

    a

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -1,1 +1,19 @@
+lista= [1,3,8,14,32,56,71]
+limite_derecho= len(lista)-1
+limite_izquierdo= 0
+numero=8
 
+def busqueda(lista,limite_derecho, limite_izquierdo, numero):  
+    index= (limite_derecho+limite_izquierdo)//2
+    if lista[index]== numero:
+        print("encontrado en:",index)
+    elif lista[index]>numero:
+        limite_derecho= index
+        busqueda(lista,limite_derecho,limite_izquierdo,numero)
+    else:
+        limite_izquierdo= index
+        busqueda(lista,limite_derecho,limite_izquierdo,numero)
+        
+        
+        
+busqueda(lista,limite_derecho,limite_izquierdo,numero)
